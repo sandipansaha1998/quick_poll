d457d26b054ffc5204ccf89f42300cc0e96904c8
Sandipan Saha <sandipansaha1998@icloud.com>
2023-07-23 11:28:37 +0530

    Update API

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://polingapi.socialise-india.in";
+const URL = "https://polling-api-lbat.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const API_ROOT = "https://polingapi.socialise-india.in/api/v1";
+const API_ROOT = "https://polling-api-lbat.onrender.com/api/v1";
 
 export const API_URLS = {
   login: () => `${API_ROOT}/user/create-session`,
